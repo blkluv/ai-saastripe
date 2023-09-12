61e147d852678b0adecf8a49f49efcd2ef9b9347
lurgi <wahoo9040@naver.com>
2023-09-12 11:36:49 +0900

    apiLimit setting

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -3,6 +3,8 @@ import { NextResponse } from "next/server";
 import OpenAI from "openai";
 import { ChatCompletionMessage } from "openai/resources/chat/index.mjs";
 
+import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
+
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -21,10 +23,17 @@ export async function POST(req: Request) {
     if (!messages)
       return new NextResponse("Messages are required", { status: 400 });
 
+    const freeTrial = await checkApiLimit();
+    if (!freeTrial) {
+      return new NextResponse("Free trial has expired", { status: 403 });
+    }
+
     const chatCompletion = await openai.chat.completions.create({
       messages: [instructionMessage, ...messages],
       model: "gpt-3.5-turbo",
     });
+
+    await increaseApiLimit();
     return NextResponse.json(chatCompletion.choices[0].message);
   } catch (error) {
     console.log("[CODE_GENERATION_ERROR]", error);
